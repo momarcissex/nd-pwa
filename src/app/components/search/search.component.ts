879bef59028b7575a3aeac2cbfa6f3c86bf9629a
momar <momar@nxtdrop.com>
2020-09-09 20:13:25 -0600

    frontend filters

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -22,8 +22,12 @@ export class SearchComponent implements OnInit {
 
   results;
 
-  nbPages: number = 1;
-  searchLimit: boolean = false;
+  nbPages: number = 1
+  searchLimit: boolean = false
+  nbHits: number = 0
+
+  categorySelected: string[] = []
+  sizeSelected: string[] = []
 
   typingTimer;
   doneTypingInterval = 1000;
@@ -72,7 +76,8 @@ export class SearchComponent implements OnInit {
       }, (err, hits: any = {}) => {
         if (err) throw err;
 
-        this.results = hits.hits;
+        this.results = hits.hits
+        this.nbHits = hits.nbHits
 
         if (hits.nbPages <= this.nbPages || hits.nbPages === 0) {
           this.searchLimit = true;
@@ -89,6 +94,43 @@ export class SearchComponent implements OnInit {
         })
       });
     }, this.doneTypingInterval);
+  }
+
+  selectCategory($event) {
+    if (this.categorySelected.indexOf($event.target.id) != -1) {
+      this.categorySelected.splice(this.categorySelected.indexOf($event.target.id), 1);
+      (document.getElementById(`${$event.target.id}`) as HTMLInputElement).classList.remove('categorySelected')
+    } else {
+      this.categorySelected.push($event.target.id);
+      (document.getElementById(`${$event.target.id}`) as HTMLInputElement).classList.add('categorySelected')
+    }
+
+    //console.log(this.categorySelected)
+  }
+
+  selectSize($event) {
+    if (this.sizeSelected.indexOf($event.target.id) != -1) {
+      this.sizeSelected.splice(this.sizeSelected.indexOf($event.target.id), 1);
+      (document.getElementById(`${$event.target.id}`) as HTMLInputElement).classList.remove('sizeSelected')
+    } else {
+      this.sizeSelected.push($event.target.id);
+      (document.getElementById(`${$event.target.id}`) as HTMLInputElement).classList.add('sizeSelected')
+    }
+
+    //console.log(this.sizeSelected)
+  }
+
+  showFilters() {
+    (document.getElementById(`filter-modal`) as HTMLInputElement).style.height = '100%'
+  }
+
+  closeFilters() {
+    (document.getElementById(`filter-modal`) as HTMLInputElement).style.height = '0'
+  }
+
+  applyFilters() {
+    this.closeFilters()
+    console.log(`Categories: ${this.categorySelected} & Sizes: ${this.sizeSelected}`)
   }
 
   moreProducts() {
