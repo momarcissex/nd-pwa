581ccc7c0d5f3b367cbf9b8740a38b0d6beca843
momar <momar@nxtdrop.com>
2021-01-30 05:45:21 -0700

    exp004: design & responsiveness

--- src/app/components/recently-viewed/recently-viewed.component.ts
+++ src/app/components/recently-viewed/recently-viewed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from 'src/app/globals';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-recently-viewed',
@@ -7,11 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecentlyViewedComponent implements OnInit {
 
-  constructor() { }
+  recently_viewed: Product[] = []
 
+  /**
+   * RecentlyViewedComponent Constructor
+   */
+  constructor(
+    private globals: Globals,
+    private product_service: ProductService
+  ) { }
+
+  /**
+   * ngOnInit function
+   */
   ngOnInit(): void {
+    this.getRecentlyViewed()
   }
 
-  /** Get users recently viewed items */
+  /**
+   * Get users recently viewed items 
+   */
+  private getRecentlyViewed(): void {
+    if (this.globals.user_data != undefined) {
+      if (this.globals.user_data.recently_viewed != undefined) {
+        this.globals.user_data.recently_viewed.forEach(product_id => {
+          this.product_service.getProductInfo(product_id).subscribe(res => {
+            this.recently_viewed.push(res)
+          },
+          err => {
+            console.error(err)
+          })
+        })
+      }
+    }
+  }
+
+  /**
+   * Track click on product with Google Analytics
+   */
+  public googAnalyticsTracking(): void {
+  }
 
 }
